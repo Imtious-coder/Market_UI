ac49e67c9a7f4702680ea4d7044a4770691413e8
Imtious-Coder <iarafat386@gmail.com>
2023-02-03 15:09:59 +0600

    Bootstrap, google fonts added. Layout created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home/>} />
+          <Route path="about" element={<About/>} />
+          <Route path="contact" element={<Contact/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
