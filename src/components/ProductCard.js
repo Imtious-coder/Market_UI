423e6c17745d8ffd038ecc32741b187de8ba5347
Imtious-Coder <iarafat386@gmail.com>
2023-03-01 21:22:33 +0600

    Product card link error fixed

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -9,9 +9,9 @@ const ProductCard = ({ grid }) => {
       <Link to="/product/:id" className="product-card-link">
         <div className="product-card position-relative">
           <div className="wishlist-icon position-absolute">
-            <Link to="/">
+            <button className="border-0 bg-transparent">
               <img src="/images/wish.svg" alt="wishlist" />
-            </Link>
+            </button>
           </div>
           <div className="product-image">
             <img src="/images/watch.jpg" className="img-fluid" alt="product_image" />
@@ -34,15 +34,15 @@ const ProductCard = ({ grid }) => {
           </div>
           <div className="action-bar position-absolute">
             <div className="d-flex flex-column gap-15">
-              <Link to="/">
+              <button className="border-0 bg-transparent">
                 <img src="/images/view.svg" alt="view"/>
-              </Link>
-              <Link to="/">
+              </button>
+              <button className="border-0 bg-transparent">
                 <img src="/images/prodcompare.svg" alt="add_cart"/>
-              </Link>
-              <Link to="/">
+              </button>
+              <button className="border-0 bg-transparent">
                 <img src="/images/add-cart.svg" alt="add_cart"/>
-              </Link>
+              </button>
             </div>
           </div>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,65 +2,64 @@ import React from 'react'
 import Marquee from 'react-fast-marquee'
 import { Link } from 'react-router-dom'
 import BlogCard from '../components/BlogCard'
+import Container from '../components/Container'
 import ProductCard from '../components/ProductCard'
 import SpecialProduct from '../components/SpecialProduct'
 
 const Home = () => {
   return (
     <>
-      <section className="home-wrapper-1 py-5">
-        <div className="container-xxl">
-          <div className="row">
-            <div className="col-6">
-              <div className="main-banner position-relative">
-                <img src="images/main-banner-1.jpg" alt="main-banner" className="img-fluid rounded-3"/>
-                <div className="main-banner-content position-absolute">
-                  <h4>SUPERCHARGED FOR PROS</h4>
-                  <h5>iPad s13+ pro</h5>
-                  <p>From $999.00 or $41.62/mo.</p>
-                  <Link to="/" className="button">BUY NOW</Link>
-                </div>
+      <Container class1="home-wrapper-1 py-5">
+        <div className="row">
+          <div className="col-6">
+            <div className="main-banner position-relative">
+              <img src="images/main-banner-1.jpg" alt="main-banner" className="img-fluid rounded-3"/>
+              <div className="main-banner-content position-absolute">
+                <h4>SUPERCHARGED FOR PROS</h4>
+                <h5>iPad s13+ pro</h5>
+                <p>From $999.00 or $41.62/mo.</p>
+                <Link to="/" className="button">BUY NOW</Link>
               </div>
             </div>
-            <div className="col-6">
-              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
-                <div className="small-banner position-relative">
-                  <img src="images/catbanner-01.jpg" alt="main-banner" className="img-fluid rounded-3"/>
-                  <div className="small-banner-content position-absolute">
-                    <h4>BEST SALE</h4>
-                    <h5>iPad s13+ pro</h5>
-                    <p>From $1699.00 <br/> or $64.62/mo.</p>
-                  </div>
+          </div>
+          <div className="col-6">
+            <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+              <div className="small-banner position-relative">
+                <img src="images/catbanner-01.jpg" alt="main-banner" className="img-fluid rounded-3"/>
+                <div className="small-banner-content position-absolute">
+                  <h4>BEST SALE</h4>
+                  <h5>iPad s13+ pro</h5>
+                  <p>From $1699.00 <br/> or $64.62/mo.</p>
                 </div>
-                <div className="small-banner position-relative">
-                  <img src="images/catbanner-03.jpg" alt="main-banner" className="img-fluid rounded-3"/>
-                  <div className="small-banner-content position-absolute">
-                    <h4>NEW ARRIVAL</h4>
-                    <h5>Buy IPad Air</h5>
-                    <p>From $599.00 or <br/> $49.91/mo. for 12 mon. *</p>
-                  </div>
+              </div>
+              <div className="small-banner position-relative">
+                <img src="images/catbanner-03.jpg" alt="main-banner" className="img-fluid rounded-3"/>
+                <div className="small-banner-content position-absolute">
+                  <h4>NEW ARRIVAL</h4>
+                  <h5>Buy IPad Air</h5>
+                  <p>From $599.00 or <br/> $49.91/mo. for 12 mon. *</p>
                 </div>
-                <div className="small-banner position-relative">
-                  <img src="images/catbanner-02.jpg" alt="main-banner" className="img-fluid rounded-3"/>
-                  <div className="small-banner-content position-absolute">
-                    <h4>15% OFF</h4>
-                    <h5>Smartwatch 7</h5>
-                    <p>Shop the latest brand <br/> styles and colors.</p>
-                  </div>
+              </div>
+                  <div className="small-banner position-relative">
+                <img src="images/catbanner-02.jpg" alt="main-banner" className="img-fluid rounded-3"/>
+                <div className="small-banner-content position-absolute">
+                  <h4>15% OFF</h4>
+                  <h5>Smartwatch 7</h5>
+                  <p>Shop the latest brand <br/> styles and colors.</p>
                 </div>
-                <div className="small-banner position-relative">
-                  <img src="images/catbanner-04.jpg" alt="main-banner" className="img-fluid rounded-3"/>
-                  <div className="small-banner-content position-absolute">
-                    <h4>FREE ENGRAVING</h4>
-                    <h5>AirPods Max</h5>
-                    <p>Hight-fidelity playback & <br/> ultra low distortion.</p>
-                  </div>
+              </div>
+              <div className="small-banner position-relative">
+                <img src="images/catbanner-04.jpg" alt="main-banner" className="img-fluid rounded-3"/>
+                <div className="small-banner-content position-absolute">
+                  <h4>FREE ENGRAVING</h4>
+                  <h5>AirPods Max</h5>
+                  <p>Hight-fidelity playback & <br/> ultra low distortion.</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </Container>
       <section className="home-wrapper-2 py-5">
         <div className="container-xxl">
           <div className="row">
