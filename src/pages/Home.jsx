7adea9aeb0ece54e49c84feec19284c07b3a8ca6
Imtious-Coder <iarafat386@gmail.com>
2023-03-22 02:29:06 +0600

    Famous products responsive complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -265,19 +265,24 @@ const Home = () => {
           <ProductCard />
         </div>
       </Container>
-      <Container class1="famous-wrapper home-wrapper-2 py-5">
+      <Container class1="famous-wrapper home-wrapper-2 py-md-5">
         <div className="row">
-          <div className="col-3">
+          <div className="col-12">
+            <h3 className="section-heading text-center text-sm-start">
+              Famous Products
+            </h3>
+          </div>
+          <div className="col-12 col-sm-6 px-5 px-sm-3 px-md-2 px-lg-3 col-md-3 mb-3">
             <div className="famous-card position-relative">
               <img className="" src="images/famouse-1.webp" alt="famous" />
               <div className="famous-card-content position-absolute px-4">
                 <h5 className="text-uppercase">Big Screen</h5>
                 <h6>Smart Watch Series 7</h6>
-                <p>From $399 or $16.62/mo. for 24 mo.*</p>
+                <p>2999 BDT Only</p>
               </div>
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-12 col-sm-6 px-5 px-sm-3 px-md-2 px-lg-3 col-md-3 mb-3">
             <div className="famous-card position-relative">
               <img className="" src="images/famouse-2.webp" alt="famous" />
               <div className="famous-card-content position-absolute px-4">
@@ -287,31 +292,29 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-12 col-sm-6 px-5 px-sm-3 px-md-2 px-lg-3 col-md-3 mb-3">
             <div className="famous-card position-relative text-dark">
               <img className="" src="images/famous-3.webp" alt="famous" />
               <div className="famous-card-content position-absolute px-4">
                 <h5 className="text-uppercase">Smartphones</h5>
                 <h6>iPhone 13 Pro.</h6>
-                <p>
-                  Now in Black. From $999.00 or $41.62/mo. for 24 mo. Footnote*
-                </p>
+                <p>159,000 BDT Only</p>
               </div>
             </div>
           </div>
-          <div className="col-3">
+          <div className="col-12 col-sm-6 px-5 px-sm-3 px-md-2 px-lg-3 col-md-3">
             <div className="famous-card position-relative text-dark">
               <img className="" src="images/famouse-4.jpeg" alt="famous" />
               <div className="famous-card-content position-absolute px-4">
                 <h5 className="text-uppercase">home speakers</h5>
                 <h6>Room-filling sound</h6>
-                <p>From $699.00 or $116.29/mo. for 14 mo.*</p>
+                <p>499 BDT Only</p>
               </div>
             </div>
           </div>
         </div>
       </Container>
-      {/* <Container class1="special-wrapper home-wrapper-2 py-5">
+      <Container class1="special-wrapper home-wrapper-2 py-5">
         <div className="row">
           <div className="col-12">
             <h3 className="section-heading">Special Products</h3>
@@ -380,7 +383,7 @@ const Home = () => {
             <BlogCard />
           </div>
         </div>
-      </Container> */}
+      </Container>
     </>
   );
 };
