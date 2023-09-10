ecba94ca731203411a3d9e6dd7d291000616799f
Imtious-Coder <iarafat386@gmail.com>
2023-09-10 20:58:14 +0600

    Header top strip bg updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ import { Link, NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <header className="header-top-strip py-3 d-none d-md-block">
+      <header className="header-top-strip py-2 d-none d-md-block">
         <div className="container-xxl">
           <div className="row">
             <div className="col-6">
@@ -20,7 +20,7 @@ const Header = () => {
               <p className="text-end text-white mb-0">
                 Hotline:{" "}
                 <a className="text-white" href="tel:+8801635501610">
-                  +8801635501610
+                  +880163*****10
                 </a>
               </p>
             </div>
