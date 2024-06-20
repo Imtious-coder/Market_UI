43c7069c7cf3ec8c5053554574851f5068c9ca2e
Imtious-Coder <iarafat386@gmail.com>
2024-06-20 21:35:56 +0600

    :art: signup api added.

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import BreadCrumb from "../components/BreadCrumb";
 import Container from "../components/Container";
@@ -6,6 +6,40 @@ import CustomInput from "../components/CustomInput";
 import Meta from "../components/Meta";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+  });
+
+  // FORM DATA FUNCTIONALITY
+  const handleFormData = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  // SIGNUP API
+  const signup = async () => {
+    console.log("Sign up", formData);
+    let responseData;
+    await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => res.json())
+      .then((data) => (responseData = data));
+
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/");
+    } else {
+      alert(responseData.error);
+    }
+  };
+
   return (
     <>
       <Meta title={"Sign up"} />
@@ -15,26 +49,36 @@ const Signup = () => {
           <div className="col-12 col-sm-7 col-md-6 col-lg-4">
             <div className="auth-card">
               <h3 className="text-center">Create Account</h3>
-              <form action="" className="d-flex flex-column gap-15 mt-4">
+              <div className="d-flex flex-column gap-15 mt-4">
                 <CustomInput
                   type="text"
-                  name="First name"
-                  placeholder="First Name"
+                  name="username"
+                  value={formData.username}
+                  handleFormData={handleFormData}
+                  placeholder="Full Name"
                 />
                 <CustomInput
-                  type="text"
-                  name="Last name"
-                  placeholder="Last Name"
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  handleFormData={handleFormData}
+                  placeholder="Email"
                 />
-                <CustomInput type="email" name="email" placeholder="Email" />
                 <CustomInput
                   type="password"
                   name="password"
+                  value={formData.password}
+                  handleFormData={handleFormData}
                   placeholder="Password"
                 />
                 <div>
                   <div className="d-flex flex-column justify-content-center align-items-center mt-3 gap-15">
-                    <button className="button border-0">Create</button>
+                    <button
+                      onClick={() => signup()}
+                      className="button border-0"
+                    >
+                      Create
+                    </button>
                     <Link to="/login">
                       <p className="mb-0 text-primary">
                         Already have an account?
@@ -42,7 +86,7 @@ const Signup = () => {
                     </Link>
                   </div>
                 </div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
